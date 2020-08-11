7b43fbca46c87f18474fa55f531dcbcdcad7686f
barblo4957 <b.blokesz@gmail.com>
2020-08-11 20:29:42 +0200

    new version

--- kodilla - testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla - testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,4 +1,22 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
+    int a;
+    int b;
+
+    public Calculator() {
+        this.a = a;
+        this.b = b;
+
+    }
+
+   public int add(int a, int b){
+        return a + b;
+   }
+
+   public int sub(int a, int b){
+        return  a - b;
+   }
+
+
 }
--- kodilla - testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
+++ kodilla - testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
@@ -1,4 +1,15 @@
 package com.kodilla.testing.calculator;
 
 public class TestingMain {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int addResult = calc.add(180, 20);
+        int subResult = calc.sub(200, 100);
+        if (addResult == 200 && subResult == 100) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!!!");
+        }
+    }
 }
+
