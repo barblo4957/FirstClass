7969c850154812d4bfeca071fc2aee075fcfaa8b
barblo4957 <b.blokesz@gmail.com>
2020-10-04 15:33:45 +0200

    poprawione

--- kodilla - testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla - testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -4,19 +4,14 @@ public class Calculator {
     int a;
     int b;
 
-    public Calculator() {
-        this.a = a;
-        this.b = b;
 
+    public int add(int a, int b) {
+        return a + b;
     }
 
-   public int add(int a, int b){
-        return a + b;
-   }
-
-   public int sub(int a, int b){
-        return  a - b;
-   }
+    public int sub(int a, int b) {
+        return a - b;
+    }
 
 
 }
